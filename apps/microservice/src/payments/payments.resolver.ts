095aa6f0400e4c6e2650e6a88e151378482ae452
BillArmsty <armsty9@gmail.com>
2024-11-09 00:27:06 +0300

    :fire: payment resolver

--- apps/microservice/src/payments/payments.resolver.ts
+++ apps/microservice/src/payments/payments.resolver.ts
@@ -1,9 +1,17 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { PaymentsService } from './payments.service';
+import { CreatePaymentInput } from './dto/create-payment.input';
+import { CreatePaymentAuth } from './dto/payment.auth';
 
 @Resolver()
 export class PaymentsResolver {
   constructor(private readonly paymentsService: PaymentsService) {}
+  @Mutation(() => CreatePaymentAuth)
+  async createPayment(
+    @Args('createPaymentInput') createPaymentInput: CreatePaymentInput,
+  ) {
+    return this.paymentsService.createPayment(createPaymentInput);
+  }
 
   @Mutation(() => String)
   async registerUrl(): Promise<string> {
